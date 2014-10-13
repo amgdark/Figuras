050f20cd5353a66b23c85f5c8500fce33a9d6ef3
amgdark <amgdark@uaz.edu.mx>
2014-10-13 14:27:59 -0500

    Agregué la complejidad ciclomática y autopep8

--- app/Figura.py
+++ app/Figura.py
@@ -1,24 +1,25 @@
 class Figura:
-	def __init__(self, args):
-		self.figura=args[0]
-		self.parametros=args[1:]
 
-	def getArea(self):
-		figuras={"Cuadrado" : self.cuadrado(), 
-				"Triangulo" : self.triangulo(), 
-				"Circulo" : self.circulo(),
-				"Trapecio" : self.trapecio()}
+    def __init__(self, args):
+        self.figura=args[0]
+        self.parametros = args[1:]
 
-		return figuras[self.figura]			
+    def getArea(self):
+        figuras={"Cuadrado": self.cuadrado(),
+                   "Triangulo": self.triangulo(),
+                   "Circulo": self.circulo(),
+                   "Trapecio": self.trapecio()}
 
-	def cuadrado(self):
-		return self.parametros[0]*self.parametros[0]
+        return figuras[self.figura]
 
-	def circulo(self):
-		return round(3.1416 * float(self.parametros[0]) ** 2, 2)
+    def cuadrado(self):
+        return self.parametros[0] * self.parametros[0]
 
-	def triangulo(self):
-		return round(float(self.parametros[0])*float(self.parametros[1])/2, 2)
+    def circulo(self):
+        return round(3.1416 * float(self.parametros[0]) ** 2, 2)
 
-	def trapecio(self):
-		return round(((float(self.parametros[0])+float(self.parametros[1]))/2*self.parametros[2]), 2)
+    def triangulo(self):
+        return round(float(self.parametros[0]) * float(self.parametros[1]) / 2, 2)
+
+    def trapecio(self):
+        return round(((float(self.parametros[0]) + float(self.parametros[1])) / 2 * self.parametros[2]), 2)
